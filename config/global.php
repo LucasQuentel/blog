95e66d0382021fe237d4335f87c6a402c5c3faf3
NowNewStart <j.gelmar@hotmail.de>
2016-02-08 23:13:37 +0100

    Added a global config file draft which can be modified by the enduser pretty easily to customize his blog.

---
+++ config/global.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Global config file
+    |--------------------------------------------------------------------------
+    |
+    | This file is used for the end user to configure the blog the way he wants.
+    |
+    */
+   
+
+   //Blog title
+   'title' => 'Blog',
+
+
+   /*
+   | Admins
+   | - Grants permission to the admin panel
+   | - Uses the database username of the specific user.
+   |
+   */
+   'admins' => [
+   				'NNS',
+   			   ],
+
+
+
+
+];
+
+
+?>
