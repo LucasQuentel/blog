7a56a93c8593618ed304021b247ad4dd0a169f70
NowNewStart <j.gelmar@hotmail.de>
2016-02-10 10:52:56 +0100

    Finished settings page (for now) with Change PW/Email. Will add more if I get more ideas.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
+use Illuminate\Html;
+use App\User;
+use Validator;
+use Hash;
 use App\Http\Requests;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 
 class UserController extends Controller
@@ -20,17 +25,39 @@ class UserController extends Controller
     }
 
     public function changepw(Request $request) {
+    	
+    	
     	$this->validate($request, [
         	'oldpw' => 'required|min:6',
         	'password' => 'required|min:6|confirmed',
     	]);
-
     	$oldpw = DB::table('users')->where('name', Auth::user()->name)->value('password');
-    	return view('settings');
+    	if (Hash::check($request->input('oldpw'), $oldpw))
+		{
+			$newpw = Hash::make($request->password);
+			DB::table('users')->where('name', Auth::user()->name)->update(array('password' => $newpw));
+			$confirmpw = "Your password has been successfully changed.";
+			return view('settings',compact('confirmpw'));
+		} else {
+    		$errorpw = "The old password is not correct.";
+    		return view('settings',compact('errorpw'));
+    	}
     }
 
 
     public function changemail(Request $request) {
-    	return view('settings');
+    	$this->validate($request, [
+    		'email' => 'required|email',
+    		'newemail' => 'required|email|confirmed',
+    		]);
+    	$oldem = DB::table('users')->where('name', Auth::user()->name)->value('email');
+    	if($oldem != $request->input('email')) {
+    		$errorem = "The old email is not correct.";
+    		return view('settings',compact('errorem'));
+    	} else {
+    		DB::table('users')->where('name', Auth::user()->name)->update(array('email' => $request->input('newemail')));	
+ 			$confirmem = "Your email has been successfully changed.";
+			return view('settings',compact('confirmem'));   		
+    	}
     }
 }
--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -9,22 +9,56 @@
         	<div class="row">
 				<div class="col-lg-5">
 					<center><h3>Change password</h3></center>
-						<form action="/settings/update/password" method="POST" class="form-horizontal" role="form">						<div class="form-group">
-									<label for="oldpw">Old Password</label>
-									<input type="password" name="oldpw" id="input" class="form-control" required="required" title="oldpw" placeholder="Old Password">
-								</div>		
-								<div class="form-group">
-									<label for="password">New Password</label>
-									<input type="password" name="password" id="input" class="form-control" required="required" title="password" placeholder="New Password">
-								</div>	
-								<div class="form-group">
-									<label for="oldpw">Repeat New Password</label>
-									<input type="password" name="password_confirmation" id="input" class="form-control" required="required" title="password_confirmation" placeholder="Repeat New Password">
-								</div>							
+							@if(isset($errorpw))
+								<div class="alert alert-danger">{{ $errorpw }}</div>
+							@elseif(isset($confirmpw))
+								<div class="alert alert-success">{{ $confirmpw }}</div>
+							@endif	
+						<form action="/settings/update/password" method="POST" class="form-horizontal" role="form">				
+
+							<div class="form-group{{ $errors->has('oldpw') ? ' has-error' : '' }}">
+                            	<label class="col-md-4 control-label">Old Password</label>
+
+                            	<div class="col-md-6">
+                                	<input type="password" class="form-control" name="oldpw">
+
+                                	@if ($errors->has('oldpw'))
+                                    	<span class="help-block">
+                                        	<strong>{{ $errors->first('oldpw') }}</strong>
+                                    	</span>
+                                	@endif
+                            	</div>
+                        	</div>	
+ 							<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            	<label class="col-md-4 control-label">New Password</label>
+
+                            	<div class="col-md-6">
+                                	<input type="password" class="form-control" name="password">
+
+                                	@if ($errors->has('password'))
+                                    	<span class="help-block">
+                                        	<strong>{{ $errors->first('password') }}</strong>
+                                    	</span>
+                                	@endif
+                            	</div>
+                        	</div>		
+                        	<div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                            	<label class="col-md-4 control-label">Confirm Password</label>
+
+                            	<div class="col-md-6">
+                                	<input type="password" class="form-control" name="password_confirmation">
+
+                                	@if ($errors->has('password_confirmation'))
+                                    	<span class="help-block">
+                                        	<strong>{{ $errors->first('password_confirmation') }}</strong>
+                                    	</span>
+                                	@endif
+                            	</div>
+                        	</div>												
             					<input type="hidden" name="_token" value="{{ csrf_token() }}">						
 								<div class="form-group">
 									<div class="col-sm-10 col-sm-offset-2">
-										<button type="submit" class="btn btn-primary">Change</button>
+										<input type="submit" class="btn btn-primary" value="Change" />
 									</div>
 								</div>
 						</form>
@@ -32,19 +66,51 @@
 				<div class="col-lg-2"></div>
 				<div class="col-lg-5">
 					<center><h3>Change E-Mail</h3></center>
+							@if(isset($errorem))
+								<div class="alert alert-danger">{{ $errorem }}</div>
+							@elseif(isset($confirmem))
+								<div class="alert alert-success">{{ $confirmem }}</div>
+							@endif						
 					<form action="/settings/update/email" method="POST" class="form-horizontal" role="form">
-								<div class="form-group">
-									<label for="oldemail">Old E-Mail</label>
-									<input type="email" name="oldem" id="input" class="form-control" required="required" title="oldem" placeholder="Old E-Mail">
-								</div>		
-								<div class="form-group">
-									<label for="email">New E-Mail</label>
-									<input type="email" name="email" id="input" class="form-control" required="required" title="email" placeholder="New E-Mail">
-								</div>	
-								<div class="form-group">
-									<label for="oldpw">Repeat New E-Mail</label>
-									<input type="email" name="email_confirmation" id="input" class="form-control" required="required" title="email_confirmation" placeholder="Repeat New E-Mail">
-								</div>							
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Old E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>		
+                        <div class="form-group{{ $errors->has('newemail') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">New E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input type="email" class="form-control" name="newemail" value="{{ old('newemail') }}">
+
+                                @if ($errors->has('newemail'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('newemail') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group{{ $errors->has('newemail_confirmation') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Repeat E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input type="email" class="form-control" name="newemail_confirmation" value="{{ old('newemail_confirmation') }}">
+
+                                @if ($errors->has('newemail_confirmation'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('newemail_confirmation') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>						
             					<input type="hidden" name="_token" value="{{ csrf_token() }}">						
 							
 					
